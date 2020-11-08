cffeb5b81dcddd76e80328f8b252949a0c2935c3
Abenezer Mamo <major.abenezer@gmail.com>
2020-11-07 17:12:09 -0800

    added version info in setup for dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
       author_email='contact@abmamo.com',
       license='MIT',
       packages=find_packages(exclude=("tests", "env")),
-      install_requires=["pandas", "fastparquet", "sqlalchemy", "pyarrow", "Faker"],
+      install_requires=["pandas==1.1.2", "sqlalchemy==1.3.20", "pyarrow==0.16.0", "Faker==4.14.2"],
       zip_safe=False
 )
