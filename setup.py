0bf9c5ffdb23ec5673f31729718b65b623cb4da4
Abenezer Mamo <major.abenezer@gmail.com>
2020-11-12 01:22:11 -0800

    updated package info in setup for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
       name='mock',
-      version='0.0.2',
+      version='0.0.3',
       description='generate fake data',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/abmamo/mock',
       author='Abenezer Mamo',
       author_email='contact@abmamo.com',
