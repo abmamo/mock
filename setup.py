18c9d9787971b224e0223f7f43b77dab0bf7c078
Abenezer Mamo <major.abenezer@gmail.com>
2020-11-16 18:45:06 -0800

    updated README

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
       name='mock',
-      version='0.0.4    ',
+      version='0.0.5',
       description='generate fake data',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -21,6 +21,12 @@ setup(
       author_email='contact@abmamo.com',
       license='MIT',
       packages=find_packages(exclude=("tests", "env")),
-      install_requires=["Faker==4.14.2", "sqlalchemy==1.3.20", "pandas==1.1.2", "pyarrow==0.16.0", "xlrd==1.2.0", "tqdm==4.51.0"],
+      install_requires=[
+          "Faker==4.14.2",
+          "sqlalchemy==1.3.20",
+          "pandas==1.1.2",
+          "pyarrow==0.16.0",
+          "xlrd==1.2.0",
+          "tqdm==4.51.0"],
       zip_safe=False
 )
